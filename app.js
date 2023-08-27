db8c9d504a6e39aed65a5ec399e02a0c8dcf3228
Huseyin Taze <huseyintaze105@gmail.com>
2023-08-28 00:15:50 +0300

    nodemon installed

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+const port = 5000;
 app.get('/', function (req, res) {
-res.send("Hello World!");
+    res.send("Hello World!");
 });
 
-app.listen(5000);
+app.listen(port, ()=>{
+    console.log(`Application running on port: ${port}`)
+});
